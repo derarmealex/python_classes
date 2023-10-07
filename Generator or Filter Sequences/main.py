06b5b52b59f94f8e4e775b12834ed177f078fb43
Alex Green <alexander.navotnyi@gmail.com>
2023-10-07 16:06:22 +0200

    Update main.py

--- Generator or Filter Sequences/main.py
+++ Generator or Filter Sequences/main.py
@@ -4,12 +4,11 @@ gen = gen_find_num.GenFindNum()
 
 lst = [1, 2, 3, 4, 17, 47, 50, 90, 101, 0]
 fin_lst = gen.find_prime(lst)
-
-#print(list(fin_lst))                               # [2, 3, 17, 47, 101]
+print(list(fin_lst))                               # [2, 3, 17, 47, 101]
 # or
-print(next(fin_lst))                               # 2
-print(next(fin_lst))                               # 3
-print(next(fin_lst))                               # 17
-print(next(fin_lst))                               # 47
-print(next(fin_lst))                               # 101
-print(next(fin_lst))                               # StopIteration
+#print(next(fin_lst))                               # 2
+#print(next(fin_lst))                               # 3
+#print(next(fin_lst))                               # 17
+#print(next(fin_lst))                               # 47
+#print(next(fin_lst))                               # 101
+#print(next(fin_lst))                               # StopIteration
